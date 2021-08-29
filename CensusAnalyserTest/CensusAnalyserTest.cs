f0e26a3508df33da894cb6caa6912475c856540b
Karan Kanchan <karan.kanchan@morningstar.com>
2021-08-29 09:32:45 +0530

    Test Case 1.2 : Given wrong file path should throw exception

--- CensusAnalyserTest/CensusAnalyserTest.cs
+++ CensusAnalyserTest/CensusAnalyserTest.cs
@@ -1,10 +1,12 @@
 using NUnit.Framework;
 using CensusAnalyser;
+using System;
 
 namespace CensusAnalyserTest
 {
     public class Tests
     {
+        const string filePath = @"C:\Users\kkancha\source\repos\StateCensusAnalyser\StateCensusData.csv";
         [SetUp]
         public void Setup()
         {
@@ -14,11 +16,24 @@ namespace CensusAnalyserTest
         public void MatchNumberOfRecords()
         {
             int expected = 29;
-            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(@"C:\Users\kkancha\source\repos\StateCensusAnalyser\StateCensusData.csv");
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(filePath);
             int actual = stateCensusAnalyser.ReadRecords();
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);           
             
-            
+        }
+        [Test]
+        public void GivenCsvFilePAth_WhenImproper_ShouldThrowException()
+        {
+            string expected = "Wrong file path or file missing";
+            try
+            {
+                StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(@"C:\Users\kkancha\source\repos\StateCensusAnalyser\StateData.csv");
+                stateCensusAnalyser.ReadRecords();
+            }
+            catch(Exception exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
         }
     }
 }
