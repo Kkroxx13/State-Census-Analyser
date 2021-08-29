2a296fd1d9fc978015324440b58b4e562fc37fd1
Karan Kanchan <karan.kanchan@morningstar.com>
2021-08-29 09:05:01 +0530

    Added: State census analyser and method to read record

---
+++ StateCensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateCensusAnalyser
+{
+    class CensusAnalyserException : Exception
+    {
+        public enum CensusExceptionType
+        {
+            file_not_found
+        }
+        CensusExceptionType exceptionType;
+        public CensusAnalyserException(CensusExceptionType exceptionType, string exceptionMessage) : base(exceptionMessage)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
